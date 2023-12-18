c01673b3e1e44cb4a5813e089e617ff2361f5144
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-12-18 17:29:22 +0200

    added params and redirect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import JokeDetails from "./components/pages/JokeDetails";
 import AddJoke from "./components/pages/AddJoke";
 import Jokes from "./components/pages/Jokes";
 
 function App() {
   return <Switch>
+    <Route path='/' exact>
+      <Redirect to='/jokes' />
+    </Route>
     <Route path='/jokes' exact>
       <Jokes />
     </Route>
