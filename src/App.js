b93dcd940ac67e9d23d4e3a94cd847f0d4537e8e
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-12-18 17:19:13 +0200

    added Router and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Switch } from "react-router-dom";
+import JokeDetails from "./components/pages/JokeDetails";
+import AddJoke from "./components/pages/AddJoke";
+import Jokes from "./components/pages/Jokes";
+
 function App() {
-  return <div></div>;
+  return <Switch>
+    <Route path='/jokes' exact>
+      <Jokes />
+    </Route>
+    <Route path='/jokes/:jokeId'>
+      <JokeDetails />
+    </Route>
+    <Route path='/add-joke'>
+      <AddJoke />
+    </Route>
+  </Switch>;
 }
 
 export default App;
