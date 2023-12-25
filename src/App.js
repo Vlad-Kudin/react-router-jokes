2d5d309905c30d15d284eae876391e999e86ddae
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-12-25 13:39:22 +0200

    added NavLinks

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import JokeDetails from "./components/pages/JokeDetails";
 import AddJoke from "./components/pages/AddJoke";
 import Jokes from "./components/pages/Jokes";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return <Switch>
-    <Route path='/' exact>
-      <Redirect to='/jokes' />
-    </Route>
-    <Route path='/jokes' exact>
-      <Jokes />
-    </Route>
-    <Route path='/jokes/:jokeId'>
-      <JokeDetails />
-    </Route>
-    <Route path='/add-joke'>
-      <AddJoke />
-    </Route>
-  </Switch>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/jokes' />
+        </Route>
+        <Route path='/jokes' exact>
+          <Jokes />
+        </Route>
+        <Route path='/jokes/:jokeId'>
+          <JokeDetails />
+        </Route>
+        <Route path='/add-joke'>
+          <AddJoke />
+        </Route>
+      </Switch>
+    </Layout>
+  )
 }
 
 export default App;
